ada4210497f0515d39af8966ae6dbdc04d224858
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-24 13:04:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route/>
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
